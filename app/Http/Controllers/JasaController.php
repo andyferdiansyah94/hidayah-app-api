f0bee23c4726260eeeb4423f2dd9ddafb8b24f2b
Andy Ferdiansyah <andyferdiansyah94@gmail.com>
2025-08-04 09:06:05 +0700

    add search and sort in jasa

--- app/Http/Controllers/JasaController.php
+++ app/Http/Controllers/JasaController.php
@@ -9,17 +9,43 @@ use Exception;
 
 class JasaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        try {
-            $jasa = Jasa::all();
-            return response()->json([
-                'message' => 'Get data jasa successfully',
-                'data' => $jasa,
-            ], 200);
-        } catch (Exception $e) {
-            return response()->json(['message' => 'Terjadi kesalahan saat mengambil data jasa', 'error' => $e->getMessage()], 500);
+        $query = Jasa::query();
+
+        if ($request->has('search')) {
+            $search = $request->input('search');
+            $query->where('name', 'like', "%{$search}%");
         }
+
+        if ($request->has('sort')) {
+            switch ($request->input('sort')) {
+                case 'az':
+                    $query->orderBy('name', 'asc');
+                    break;
+                case 'za':
+                    $query->orderBy('name', 'desc');
+                    break;
+                case 'oldest':
+                    $query->orderBy('created_at', 'asc');
+                    break;
+                case 'latest':
+                    $query->orderBy('created_at', 'desc');
+                    break;
+                default:
+                    $query->orderBy('created_at', 'desc');
+                    break;
+            }
+        } else {
+            $query->orderBy('updated_at', 'asc');
+        }
+
+        $barangs = $query->get();
+
+        return response()->json([
+            'message' => 'Get data jasa successfully',
+            'data' => $barangs,
+        ], 200);
     }
 
     public function store(Request $request)
